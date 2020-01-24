3299477f35628351432b0deb6d25fda54147e521
calm-fork-7535 <komald.4@gmail.com>
2020-01-24 02:52:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -32,6 +32,28 @@ def map_to_no_change(source_array)
   return new
 end
 
+
+def map_to_double(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] * 2 )
+    i += 1
+  end
+  return new
+end
+
+def map_to_square(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] * source_array[i] )
+    i += 1
+  end
+  return new
+end
+
+
 # def map_to_no_change(source_array)
 # source_array
 # end
@@ -53,6 +75,26 @@ end
 # def reduce_to_all_true(array)
 #   array.reduce { |beTrue, beFalse| beTrue && beFalse}
 # end
+
+def reduce_to_total(source_array, starting_point=0)
+  new = starting_point
+  i = 0
+  while i < source_array.length do
+    new += source_array[i]
+    i += 1
+  end
+  return new
+end
+
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return false if !source_array[i]
+    i += 1
+  end
+  return true
+end
+
 
 def reduce_to_any_true(array)
   i = 0
