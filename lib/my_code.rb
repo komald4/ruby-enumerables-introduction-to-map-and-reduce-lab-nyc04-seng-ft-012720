57bb174fd00b0a9a8ad1995d6021110206a320ca
calm-fork-7535 <komald.4@gmail.com>
2020-01-24 02:50:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,27 +9,38 @@ def map_to_negativize(source_array)
   new_array
 end
 
-def map_to_no_change(source_array)
-source_array
+
+
+
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return false if !source_array[i]
+    i += 1
+  end
+  return true
 end
-
- def map_to_double(source_array)
-source_array.map { |element| element * 2 }
-end
-
-
- def map_to_square(source_array)
-source_array.map { |element| element ** 2 }
- end
-
-
- def reduce_to_total(array, starting_point = 0)
-   array.reduce(starting_point) { |sum, num| sum + num}
-end
-
-def reduce_to_all_true(array)
-  array.reduce { |beTrue, beFalse| beTrue && beFalse}
-end
+# def map_to_no_change(source_array)
+# source_array
+# end
+#
+#  def map_to_double(source_array)
+# source_array.map { |element| element * 2 }
+# end
+#
+#
+#  def map_to_square(source_array)
+# source_array.map { |element| element ** 2 }
+#  end
+#
+#
+#  def reduce_to_total(array, starting_point = 0)
+#    array.reduce(starting_point) { |sum, num| sum + num}
+# end
+#
+# def reduce_to_all_true(array)
+#   array.reduce { |beTrue, beFalse| beTrue && beFalse}
+# end
 
 def reduce_to_any_true(array)
   i = 0
